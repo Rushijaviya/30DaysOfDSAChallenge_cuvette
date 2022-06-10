5dd008c1bec6d044a07102fedeea4ddecc3b4ba3
unknown <201951074@iiitvadodara.ac.in>
2022-06-10 20:04:11 +0530

    folder made for different language

---
+++ C++/02_Maximum_Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& v) {
+        int sum =v[0];
+        int maxm = v[0];
+        int n = v.size();
+        for(int i=1;i<n;i++){
+            if(sum+v[i]<v[i]){
+                sum=v[i];
+                maxm=max(sum,maxm);
+            }else{
+                sum+=v[i];
+                maxm=max(sum,maxm);
+            }
+        }
+        return max(sum,maxm);
+    }
+};
