39d581b307743c666836c926633f792e804e405c
Siddharth Singh <95075464+siddharthsinghvats@users.noreply.github.com>
2022-06-10 19:05:14 +0530

    Create 1_Sort_Array_Of_012.cpp

---
+++ 1_Sort_Array_Of_012.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        int fre[3]={0,0,0};
+        for(int i =0;i<n;i++) fre[a[i]]++;
+        int i;
+        for(i=0;i<fre[0];i++) a[i]=0;
+        for(;fre[1]--;i++) a[i]=1;
+        for(;i<n;i++) a[i]=2;
+    }
+    
+};
