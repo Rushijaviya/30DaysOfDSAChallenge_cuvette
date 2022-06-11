cadb4027ca3987acdfde31db81772a07bebc552e
Siddharth Singh <95075464+siddharthsinghvats@users.noreply.github.com>
2022-06-11 15:59:32 +0530

    Solved day 3 problem

---
+++ C++/03_Implement_Stack_Using_Queues.cpp
@@ -0,0 +1,45 @@
+class MyStack {
+public:
+    queue<int> q1,q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+       while(q1.size()>1){
+           q2.push(q1.front());
+           q1.pop();
+       } 
+        int x = q1.front();
+        q1.pop();
+        while(q2.size()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return x;
+        
+    }
+    
+    int top() {
+        while(q1.size()>1){
+           q2.push(q1.front());
+           q1.pop();
+       } 
+        int x = q1.front();
+        q2.push(x);
+        q1.pop();
+        while(q2.size()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return x;
+    }
+    
+    bool empty() {
+        return !q1.size();
+    }
+};
