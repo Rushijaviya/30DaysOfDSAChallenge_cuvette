46cc05938ff4527f5f48257d7a975b296cb53402
unknown <201951074@iiitvadodara.ac.in>
2022-07-04 09:02:21 +0530

    Sort a stack

---
+++ Python/25_Sort a stack.py
@@ -0,0 +1,28 @@
+# Sort a stack 
+# https://practice.geeksforgeeks.org/problems/sort-a-stack/1
+
+class Solution:
+    # your task is to complete this function
+    # function sort the stack such that top element is max
+    # funciton should return nothing
+    # s is a stack
+    def sorted(self, s):
+        # Code here
+        s.sort()
+        s=s[::-1]
+
+#{ 
+#  Driver Code Starts
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.sorted(arr)
+        for e in range(len(arr)):
+            print(arr.pop(), end=" ")
+        print()
+
+
+# } Driver Code Ends
